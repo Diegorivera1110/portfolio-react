89bf70c75d315ad30d2a9a7cc5cdf0010d18e2e9
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-08-05 20:16:03 -0700

    added resume, contact, and about components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Redirect, HashRouter } from "react-router-dom";
+import Navigation from "../Navigation";
+import About from "../About";
+import Project from "../Project";
+import Contact from "../Contact";
 
-function Header() {
-    return (
-        <header>
-            
-        </header>
-    )
+
+
+class Header extends Component {
+    Header() {
+        return (
+            <HashRouter>
+                <header>
+                    <Navigation />
+                </header>
+
+                <div className="page">
+                    <Route exact path="/" render={() => <Redirect to="/about" />} />
+                    <Route path="/about" component={About}></Route>
+                    <Route path="/contact" component={Contact}></Route>
+                    <Route path="/project" component={Project}></Route>
+                </div>
+            </HashRouter>
+        )
+    }
 }
 
 export default Header;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,18 +1,18 @@
 import React, { useEffect } from "react";
-import { capitalizeFirstLetter } from "../../../utils/helpers";
+// import { capitalizeFirstLetter } from "../../../utils/helpers";
 
-function Nav(props) {
-    const {
-        categories = [],
-        setCurrentCategory,
-        contactSelected,
-        currentCategory,
-        setContactSelected,
-    } = props;
+function Nav() {
+    // const {
+    //     categories = [],
+    //     setCurrentCategory,
+    //     contactSelected,
+    //     currentCategory,
+    //     setContactSelected,
+    // } = props;
 
-    useEffect(() => {
-        document.title = capitalizeFirstLetter(currentCategory.name);
-    }, [currentCategory]);
+    // useEffect(() => {
+    //     document.title = capitalizeFirstLetter(currentCategory.name);
+    // }, [currentCategory]);
 
     return(
         <header className="flex-row px-1">
@@ -27,6 +27,9 @@ function Nav(props) {
 
             <nav>
                 <ul className="flex-row">
+                    <li className={`mx-2 ${contactSelected && "navActive"}`}>
+                        <a href="#about" onClick={() => setContactSelected(false)}>About Me</a>
+                    </li>
 
                 </ul>
             </nav>
